410013ede70bdc2cfda0f00f806b082410ec26b0
Argenic <simon.zwaanenburg@gmail.com>
2024-12-01 10:39:54 +0100

    Create main.cpp

---
+++ 2024/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "day01.hpp"
+// Include other days as needed
+
+int main() {
+    std::cout << "Advent of Code 2024" << std::endl;
+
+    // Call functions for each day's solution
+    day01part01();
+    day01part02();
+    //day02part01();
+    //day02part02();
+    // Add more as needed
+
+    return 0;
+}
