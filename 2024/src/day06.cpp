e926d067428b31193f65f0b34bda1fd583779b18
Argenic <simon.zwaanenburg@gmail.com>
2024-12-06 17:40:55 +0100

    Cleanup
    Day6 cleanup

--- 2024/src/day06.cpp
+++ 2024/src/day06.cpp
@@ -6,6 +6,7 @@
 #include "utils.hpp"
 #include <unordered_map>
 #include <set>
+#include <functional>
 
 // Function to print the map
 void printMap(const std::vector<std::vector<char>>& map) {
@@ -46,6 +47,34 @@ std::pair<int, int> getNextLocation(const std::pair<int, int>& location, int dir
     return nextLocation;
 }
 
+// Common function to simulate the guard's path
+std::vector<std::pair<int, int>> simulatePath(std::vector<std::vector<char>>& map, std::pair<int, int> location, 
+                                              std::function<void(std::pair<int, int>&, int&, bool&)> onBlocked = nullptr) {
+    std::vector<std::pair<int, int>> path;
+    int direction = 0;
+    bool patrolling = true;
+
+    while (patrolling) {
+        auto nextLocation = getNextLocation(location, direction);
+
+        if (nextLocation.first < 0 || nextLocation.first >= map.size() || 
+            nextLocation.second < 0 || nextLocation.second >= map[0].size()) {
+            patrolling = false;
+        } else if (map[nextLocation.first][nextLocation.second] == '#') {
+            if (onBlocked) {
+                onBlocked(location, direction, patrolling);
+            } else {
+                direction = (direction + 1) % 4;
+            }
+        } else {
+            location = nextLocation;
+            map[location.first][location.second] = 'X';
+            path.push_back(location);
+        }
+    }
+    return path;
+}
+
 // Part 1 of Day 6
 void day06part01() {
     std::cout << "\nDay 6 Part 1:\nFollow the path of the guard\n";
@@ -53,28 +82,9 @@ void day06part01() {
 
     std::vector<std::vector<char>> map;
     std::pair<int, int> location;
-    std::vector<std::pair<int, int>> path;
 
     initializeMapAndLocation(input, map, location);
-    path.push_back(location);
-
-    int direction = 0;
-    bool patrolling = true;
-
-    while (patrolling) {
-        auto nextLocation = getNextLocation(location, direction);
-
-        if (nextLocation.first < 0 || nextLocation.first >= map.size() || 
-            nextLocation.second < 0 || nextLocation.second >= map[0].size()) {
-            patrolling = false;
-        } else if (map[nextLocation.first][nextLocation.second] == '#') {
-            direction = (direction + 1) % 4;
-        } else {
-            location = nextLocation;
-            map[location.first][location.second] = 'X';
-            path.push_back(location);
-        }
-    }
+    auto path = simulatePath(map, location);
 
     std::set<std::pair<int, int>> uniquePath(path.begin(), path.end());
     std::cout << "Total: " << uniquePath.size() << std::endl;
@@ -82,35 +92,16 @@ void day06part01() {
 
 // Part 2 of Day 6
 void day06part02() {
-    std::cout << "\nDay 6 Part 2:\nHow many loops can i force the guard to walk?\n";
+    std::cout << "\nDay 6 Part 2:\nHow many loops can I force the guard to walk?\n";
     const auto input = utils::readLines("../2024/data/day06.txt");
 
     std::vector<std::vector<char>> map;
     std::pair<int, int> location;
-    std::vector<std::pair<int, int>> path;
 
     initializeMapAndLocation(input, map, location);
     auto startLocation = location;
     auto originalMap = map;
-    path.push_back(location);
-
-    int direction = 0;
-    bool patrolling = true;
-
-    while (patrolling) {
-        auto nextLocation = getNextLocation(location, direction);
-
-        if (nextLocation.first < 0 || nextLocation.first >= map.size() || 
-            nextLocation.second < 0 || nextLocation.second >= map[0].size()) {
-            patrolling = false;
-        } else if (map[nextLocation.first][nextLocation.second] == '#') {
-            direction = (direction + 1) % 4;
-        } else {
-            location = nextLocation;
-            map[location.first][location.second] = 'X';
-            path.push_back(location);
-        }
-    }
+    auto path = simulatePath(map, location);
 
     std::set<std::pair<int, int>> uniquePath(path.begin(), path.end());
     int loops = 0;
@@ -123,27 +114,17 @@ void day06part02() {
         auto copyLocation = startLocation;
         std::unordered_map<std::string, std::vector<int>> blockers;
 
-        bool patrolling = true;
-        direction = 0;
-        while (patrolling) {
-            auto nextLocation = getNextLocation(copyLocation, direction);
-
-            if (nextLocation.first < 0 || nextLocation.first >= copyMap.size() || 
-                nextLocation.second < 0 || nextLocation.second >= copyMap[0].size()) {
-                patrolling = false;
-            } else if (copyMap[nextLocation.first][nextLocation.second] == '#') {
-                direction = (direction + 1) % 4;
-                std::string coordinates = std::to_string(nextLocation.first) + "," + std::to_string(nextLocation.second);
-                if (blockers[coordinates].end() != std::find(blockers[coordinates].begin(), blockers[coordinates].end(), direction)) {
-                    loops++;
-                    patrolling = false;
-                }
-                blockers[coordinates].push_back(direction);
-            } else {
-                copyMap[nextLocation.first][nextLocation.second] = 'X';
-                copyLocation = nextLocation;
+        auto onBlocked = [&](std::pair<int, int>& loc, int& dir, bool& patrol) {
+            dir = (dir + 1) % 4;
+            std::string coordinates = std::to_string(loc.first) + "," + std::to_string(loc.second);
+            if (blockers[coordinates].end() != std::find(blockers[coordinates].begin(), blockers[coordinates].end(), dir)) {
+                loops++;
+                patrol = false;
             }
-        }
+            blockers[coordinates].push_back(dir);
+        };
+
+        simulatePath(copyMap, copyLocation, onBlocked);
     }
 
     std::cout << "Possible loops: " << loops << std::endl;
